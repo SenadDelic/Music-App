32cb17b5d74e944b426ca9fa20450a39a4c2f2ad
SenadDelic <dellic.senad@gmail.com>
2020-05-20 22:07:11 +0200

    Added constants

--- src/database/Constants.java
+++ src/database/Constants.java
@@ -3,5 +3,20 @@ package database;
 public class Constants {
     public static final String DB_NAME = "music.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
-    public static final String TIME_ZONE_ERROR = "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC";
+
+    public static final String TABLE_ALBUMS = "albums";
+    public static final String COLUMN_ALBUM_ID = "_id";
+    public static final String COLUMN_ALBUM_NAME = "name";
+    public static final String COLUMN_ALBUM_ARTIST = "artist";
+
+    public static final String TABLE_ARTIST = "artists";
+    public static final String COLUMN_ARTIST_ID = "_id";
+    public static final String COLUMN_ARTIST_NAME = "name";
+
+    public static final String TABLE_SONGS = "songs";
+    public static final String COLUMN_SONG_ID = "_id";
+    public static final String COLUMN_SONG_TRACK = "track";
+    public static final String COLUMN_SONG_TITLE = "title";
+    public static final String COLUMN_SONG_ALBUM = "album";
+
 }
